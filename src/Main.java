0575ef68e948efc99a243ae1f928dd891b03ecd5
Abing <qawitherev@gmail.com>
2022-07-07 21:48:00 +0800

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+//        linkedlist for java
+        LinkedList<String> cars = new LinkedList<>();
+//        use add() method to add items
+        cars.add("BMW");
+        cars.add("Proton");
+        cars.add("Perodua");
+        cars.add("Toyota");
+        cars.add("Honda");
+//        System.out.println("The whole linked list: " + cars);
+
+//        use addFirst() to add items at the beginning of the list
+        cars.addFirst("Mercedes");
+        System.out.println(cars);
+    }
+}
